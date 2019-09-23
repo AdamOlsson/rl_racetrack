4f1f41630ae1693a5e635e1b6ede951a32310e1d
AdamOlsson <Olsson.95_@hotmail.com>
2019-09-23 10:02:40 +0200

    Play episode no longer dependet on a uniform policy

--- off_policy_mc_control.py
+++ off_policy_mc_control.py
@@ -7,10 +7,12 @@ def play_episode(env, policy):
     episode =[]
     state = env.reset()
     while True:
-        available_actions = env.get_actions()
 
-        # TODO: Fix so a none uniform policy will work as well
-        action = np.random.choice(available_actions) # works if policy has uniform probability over actions
+        available_actions = env.get_actions()
+        # For each available action, get respective probability
+        action_probs = [policy[state, x, y] for (x,y) in available_actions]
+        
+        action = np.random.choice(available_actions, p=action_probs)
 
         next_state, reward, game_over, info = env.step(action)
         episode.append(state, action, reward)
@@ -19,22 +21,25 @@ def play_episode(env, policy):
             break
     return episode
 
-def mc_control(env, iterations=1000):
+def mc_control(env, gamma=0.01, iterations=1000):
     
     Q = defaultdict(float)
     C = defaultdict(float)
     target_policy = defaultdict(float)
 
     for e in range(iterations):
-        behaviour_policy = np.ones([env.nS, env.nA])/env.nA # no need to recreate every episode
+        behaviour_policy = np.ones([env.nS, env.action_space[0], env.action_space[1]])/env.nA # no need to recreate every episode
         
         episode = play_episode(env, behaviour_policy)
 
-        G = 0
-        W = 1
+        G = 0.0
+        W = 1.0
         
-        for (s,a,r) in episode:
-            pass
+        for (state, action, reward) in episode:
+            G = gamma*G + reward
+            C[state, action] += W
+            Q[state, action] += (W/C[state, action])*(G - Q[state, action])
+            #target_policy[state, action] = np.argmax(Q[state])
 
 
 
